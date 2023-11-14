23e015a8a3aeef7e7dcd4e1bb901f82f99b1dce8
wlangley512 <wlangley512@gmail.com>
2023-11-14 15:31:29 -0600

    consecutive negative frames check

--- servetrack.py
+++ servetrack.py
@@ -191,14 +191,16 @@ while True:
                     #    cv2.putText(img, f'le epic serve detected', (max(0, x1), max(30, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 if currentClass == "ball":
                     cv2.putText(img, f'{total_mag}', (max(30, x1), max(30, y1)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    
-        total_mag = line_render(center_points, img, line_color)
+            # Only process every other frame
+        if count % 2 == 0:
+            total_mag = line_render(center_points, img, line_color) 
+        #total_mag = line_render(center_points, img, line_color)
         #Draw line
         #if len(center_points)>=2:
         #    for i in range (1, len(center_points)):
         #        cv2.line(img, center_points[i - 1], center_points[i], line_color, 2)
     
-    
+    count+=1
     final.write(img)
     #cv2.imshow("Image", img)
     cv2.waitKey(1)
