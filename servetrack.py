6433fc1d8261df6ea7a0f5549c08bbf0b7af3f51
wlangley512 <wlangley512@gmail.com>
2023-11-14 14:26:58 -0600

    change calculate_angle

--- servetrack.py
+++ servetrack.py
@@ -113,8 +113,8 @@ def line_render(points, img, line_color):
 
         if points[i - 1] is None or points[i] is None:
             continue
-        diff_x = points[i-3][0] - points[i][0]
-        diff_y = points[i-3][1] - points[i][1]
+        diff_x = points[i-5][0] - points[i][0]
+        diff_y = points[i-5][1] - points[i][1]
         
         bal_diff_y = diff_y / balance
         bal_diff_x = diff_x / balance 
