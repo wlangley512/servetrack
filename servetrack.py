4af996426c08c82fa9a6df90bf2d24b1646e6e03
wlangley512 <wlangley512@gmail.com>
2023-11-14 19:04:22 -0600

    consecutive negative frames check

--- servetrack.py
+++ servetrack.py
@@ -114,7 +114,7 @@ def line_render(points, img, line_color):
 
         if points[i - 1] is None or points[i] is None:
             continue
-        diff_x = points[i-2][0] - points[i][0]
+        diff_x = points[i-1][0] - points[i][0]
         diff_y = points[i-1][1] - points[i][1]
         
         bal_diff_y = diff_y / balance
